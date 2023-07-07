e9f64bf7cd3c3e5938d10794b3b296f225df14ed
Yoonzero <playlist808@kakao.com>
2023-07-07 14:26:53 +0900

    댓글, 좋아요 모델 파일 구성

--- migrations/20230707051914-create-likes.js
+++ migrations/20230707051914-create-likes.js
@@ -2,8 +2,8 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
     async up(queryInterface, Sequelize) {
-        await queryInterface.createTable('Comments', {
-            commentId: {
+        await queryInterface.createTable('Likes', {
+            likeId: {
                 allowNull: false,
                 autoIncrement: true,
                 primaryKey: true,
@@ -16,7 +16,7 @@ module.exports = {
                     model: 'Users', // Users 모델을 참조합니다.
                     key: 'userId', // Users 모델의 userId를 참조합니다.
                 },
-                onDelete: 'CASCADE', // 만약 Users 모델의 userId가 삭제되면, Comments 모델의 데이터가 삭제됩니다.
+                onDelete: 'CASCADE', // 만약 Users 모델의 userId가 삭제되면, Likes 모델의 데이터가 삭제됩니다.
             },
             PostId: {
                 allowNull: false, // NOT NULL
@@ -25,22 +25,21 @@ module.exports = {
                     model: 'Posts', // Posts 모델을 참조합니다.
                     key: 'postId', // Posts 모델의 postId를 참조합니다.
                 },
-                onDelete: 'CASCADE', // 만약 Posts 모델의 postId가 삭제되면, Comments 모델의 데이터가 삭제됩니다.
+                onDelete: 'CASCADE', // 만약 Posts 모델의 postId가 삭제되면, Likes 모델의 데이터가 삭제됩니다.
             },
-            content: {
+            Nickname: {
+                allowNull: false,
                 type: Sequelize.STRING,
-            },
-            createdAt: {
-                allowNull: false,
-                type: Sequelize.DATE,
-            },
-            updatedAt: {
-                allowNull: false,
-                type: Sequelize.DATE,
+                references: {
+                    model: 'Users',
+                    key: 'nickname',
+                },
+                onDelete: 'CASCADE',
+                onUpdate: 'CASCADE',
             },
         });
     },
     async down(queryInterface, Sequelize) {
-        await queryInterface.dropTable('Comments');
+        await queryInterface.dropTable('Likes');
     },
 };
---
+++ models/likes.js
@@ -0,0 +1,66 @@
+'use strict';
+const { Model } = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+    class Likes extends Model {
+        /**
+         * Helper method for defining associations.
+         * This method is not a part of Sequelize lifecycle.
+         * The `models/index` file will call this method automatically.
+         */
+        static associate(models) {
+            // define association here
+            this.belongsTo(models.Users, {
+                targetKey: 'userId',
+                foreignKey: 'UserId',
+            });
+
+            this.belongsTo(models.Posts, {
+                targetKey: 'postId',
+                foreignKey: 'PostId',
+            });
+        }
+    }
+    Likes.init(
+        {
+            likeId: {
+                allowNull: false,
+                autoIncrement: true,
+                primaryKey: true,
+                type: DataTypes.INTEGER,
+            },
+            UserId: {
+                allowNull: false, // NOT NULL
+                type: DataTypes.INTEGER,
+                references: {
+                    model: 'Users', // Users 모델을 참조합니다.
+                    key: 'userId', // Users 모델의 userId를 참조합니다.
+                },
+                onDelete: 'CASCADE', // 만약 Users 모델의 userId가 삭제되면, Likes 모델의 데이터가 삭제됩니다.
+            },
+            PostId: {
+                allowNull: false, // NOT NULL
+                type: DataTypes.INTEGER,
+                references: {
+                    model: 'Posts', // Posts 모델을 참조합니다.
+                    key: 'postId', // Posts 모델의 postId를 참조합니다.
+                },
+                onDelete: 'CASCADE', // 만약 Posts 모델의 postId가 삭제되면, Likes 모델의 데이터가 삭제됩니다.
+            },
+            Nickname: {
+                allowNull: false,
+                type: DataTypes.STRING,
+                references: {
+                    model: 'Users',
+                    key: 'nickname',
+                },
+                onDelete: 'CASCADE',
+                onUpdate: 'CASCADE',
+            },
+        },
+        {
+            sequelize,
+            modelName: 'Likes',
+        }
+    );
+    return Likes;
+};
