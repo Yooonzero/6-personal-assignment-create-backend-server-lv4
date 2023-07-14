8995da884ccef1adb872618f6cecf474fbd04c29
Yoonzero <playlist808@kakao.com>
2023-07-14 17:27:06 +0900

    댓글 작성 예외처리

--- routes/comments.router.js
+++ routes/comments.router.js
@@ -1,24 +1,27 @@
 const express = require('express');
 const router = express.Router();
-const { Comments } = require('../models');
+const { Posts, Comments } = require('../models');
 const authMiddleware = require('../middlewares/auth-middleware.js');
 
-// 댓글 작성, 조회, 수정, 삭제
-
 // 댓글 작성
-// - 로그인 토큰을 검사하여, 유효한 토큰일 경우에만 댓글 작성 가능
-// - 댓글 내용을 비워둔 채 댓글 작성 API를 호출하면 "댓글 내용을 입력해주세요" 라는 메세지를 return하기
-// - 댓글 내용을 입력하고 댓글 작성 API를 호출한 경우 작성한 댓글을 추가하기
 router.post('/posts/:postId/comments', authMiddleware, async (req, res) => {
     const { postId } = req.params;
     const { content } = req.body;
     const { userId } = res.locals.user;
 
-    // 댓글 내용을 비워두었을 때
     try {
-        // if (!content) {
-        //     return res.status(412).json({ errorMessage: '댓글 내용을 입력해주세요' });
-        // } 현재 서버 지연 오류
+        // 해당하는 게시글이 없는 경우
+        const post = await Posts.findOne({ where: { postId } });
+        if (!post) {
+            return res.status(404).json({ errorMessage: '존재하지 않는 게시글입니다.' });
+        }
+
+        // 댓글 형식 확인
+        if (content.length < 1) {
+            return res.status(412).json({ errorMessage: '댓글 내용을 입력해주세요' });
+        }
+
+        // 댓글 생성
         await Comments.create({ UserId: userId, PostId: postId, content });
         return res.status(200).json({ message: '댓글 작성에 성공하였습니다' });
     } catch (err) {
